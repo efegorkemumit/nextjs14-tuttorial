2fadf7659f8230aa12633250d3b1ca7924cde6a7
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-14 17:10:39 +0300

    Static Page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Dashboard bileşeni, kullanıcı panelinin ana görüntüsünü oluşturur.
+// http://localhost:3000/dashboard  statik bir sayfadır
+const dashboard = () => {
+  return (
+    <div>dashboard</div>
+  )
+}
+
+export default dashboard
---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Settings bileşeni, kullanıcı panelinin ana görüntüsünü oluşturur.
+// http://localhost:3000/dashboard/settings  statik bir sayfadır
+
+const Settings = () => {
+  return (
+    <div>Settings</div>
+  )
+}
+
+export default Settings
