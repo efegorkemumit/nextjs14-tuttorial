fcfd5b470c595b1a55224759fcfe5bb1373e0297
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-14 17:42:17 +0300

    Dinamik Page

---
+++ app/doc/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Doc Sayfası Page bileşeni, kullanıcı panelinin ana görüntüsünü oluşturur.
+// http://localhost:3000/doc  statik bir sayfadır
+const page = () => {
+  return (
+    <div>Introduction  Document</div>
+  )
+}
+
+export default page
---
+++ app/doc/routing/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Doc/routing  Page bileşeni, kullanıcı panelinin ana görüntüsünü oluşturur.
+// http://localhost:3000/doc/routing  statik bir sayfadır
+
+const page = () => {
+  return (
+    <div>Routting</div>
+  )
+}
+
+export default page
