62932a72ffea2fe48211d46273f68d7b0fac75ca
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-14 23:29:41 +0300

    Temiz Başlangıç

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return (
    <>
    
-   <div className="bg-red-600">
+   <div className="bg-red-600 text-4xl">
 
-    Deneme Görkem
+    Youtube Efe Görkem Ümit Abone Ol
    </div>
      </>
   );
