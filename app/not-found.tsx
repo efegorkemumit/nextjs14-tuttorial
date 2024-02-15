eca5f38769ed08be01a5c388ce2487b34905cc69
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-16 00:17:33 +0300

    Custom NotFoundPage

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+///  Custom Notfoundpage için bu sayfa eklenir.
+const NotFoundPage = () => {
+  return (
+    <div>NotFoundPage</div>
+  )
+}
+
+export default NotFoundPage
