2ca6b97d1ce58d5bbd72f12c1f5ab85078db9862
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-15 19:12:27 +0300

    temiz başlangıç

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,21 @@
-import Image from "next/image"; // Next.js'de resim optimizasyonu için Image bileşenini içe aktarıyoruz.
-import Link from 'next/link'; // Next.js'de sayfalar arası gezinme için Link bileşenini içe aktarıyoruz.
+import Image from "next/image";
+import Link from 'next/link'
 
-// Ana bileşenimiz olan Home fonksiyonu. Bu fonksiyon, ana sayfamızı oluşturuyor.
+
+///  site klasörü sadece guruplar yaygın bir kullanımdır ana sayfa site klasörüne taşınır.
+// http://localhost:3000 sayfa urli budur
+
+
 export default function Home() {
   return (
    <>
-   {/* Ana sayfanın üst kısmı için bir kapsayıcı div. */}
+   
    <div className="bg-red-600 text-4xl">
-    Youtube Efe Görkem Ümit Abone Ol {/* Bir başlık veya metin */}
+
+    Youtube Efe Görkem Ümit Abone Ol
    </div>
 
-   {/* Ana sayfanın içeriğini belirten bir div. */}
-   <div>Home </div>
 
-   {/* Ana sayfanın alt kısmı için bir kapsayıcı div ve içinde Link bileşenleri ile sayfalar arası gezinme bağlantıları */}
-   <div className="flex gap-8">
-     {/* Home sayfasına yönlendiren bir Link */}
-     <Link href="/">Home</Link>
-     {/* Dashboard sayfasına yönlendiren bir Link */}
-     <Link href="/dashboard">Dashboard</Link>
-     {/* Product sayfasına yönlendiren bir Link */}
-     <Link href="/product">Product</Link>
-   </div>
-   </>
+     </>
   );
 }
