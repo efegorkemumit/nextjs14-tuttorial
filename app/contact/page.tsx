d2ef729012ae0394b1d64a4dad35bd88c300577c
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-16 16:53:17 +0300

    metadata

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+
+export const metadata: Metadata = {
+  title: "Contact", // Sayfa başlığı
+  description: "A contact page", // Sayfa açıklaması
+};
+
+
 // Contact sayfası
 const ContactPage = () => {
   return (
